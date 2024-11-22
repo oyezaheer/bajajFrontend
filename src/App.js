400c3800f874af7ebb1e589e910d5e73b5b95116
oyezaheer <zaheer286khan@gmail.com>
2024-11-22 18:21:05 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import JsonProcessor from './components/JsonProcessor.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+      </header> */}
+      <header>
+        BAJAJ Api Round
       </header>
       <main className="App-main">
         <JsonProcessor />
