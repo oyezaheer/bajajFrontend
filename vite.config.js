41cb1b69deb9c634a968181f7e061c0e18290f80
oyezaheer <zaheer286khan@gmail.com>
2024-11-22 20:48:38 +0530

    make changes

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "tailwindcss";
-
-
-// https://vite.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     '/bfhl': {
-  //       target: 'https://bajaj-backend-c2r3.vercel.app',
-  //       changeOrigin: true,
-  //       secure: false, // Disable if the target server has self-signed certificates
-  //     },
-  //   },
-  // },
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  css:{
-    postcss:{
-      plugins:[tailwindcss()],
+    plugins: [react()],
+    build: {
+      outDir: 'build' // Change the output directory to "build"
+    },
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
+      }
     }
-  }
-})
+  });
+  
