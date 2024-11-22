3961c852e084a259acd474c402898967ecd0471a
oyezaheer <zaheer286khan@gmail.com>
2024-11-22 21:08:32 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,8 @@ function App() {
   return (
     <div className="min-h-screen bg-teal-50 text-gray-900 flex items-center justify-center p-4">
       <div className="w-full max-w-2xl space-y-8">
-        <h1 className="text-3xl font-bold text-center text-green-600">BFHL API Tester</h1>
+        <h1 className="text-3xl font-bold text-center text-green-600">Bajaj Api Round</h1>
+        <h2>0101CS211136</h2>
   
         <div className="space-y-4 bg-white p-6 rounded-xl shadow-lg">
           <div className="space-y-2">
